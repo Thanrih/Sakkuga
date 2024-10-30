be6dad41e96b2abaec9b6eeab5540b50b5f17cd0
Jonathan <107511723+Thanrih@users.noreply.github.com>
2024-10-30 13:24:28 -0300

    Add files via upload

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:sakugaacaptors/assets/card.dart';
 import 'package:sakugaacaptors/pages/homepage.dart';
-import '../providers/provider_favorites.dart';
 import 'package:flutter/cupertino.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -15,13 +13,50 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   String searchQuery = '';
 
-  Future<List<Map<String, dynamic>>> fetchHistoryData() async {
-    final response = await supabase.from('Histórico').select('id_history');
-    return List<Map<String, dynamic>>.from(response);
+  Future<List<Map<String, dynamic>>> _data() async {
+    final userId = supabase.auth.currentUser?.id;
+    print(userId);
+    if (userId == null) {
+      print('Usuário não autenticado.');
+      return [];
+    }
+
+    try {
+      // Realiza a consulta em "Histórico" para obter os IDs das obras do usuário.
+      final historyResponse = await supabase
+          .from('Histórico')
+          .select('id_history')
+          .eq('userId', userId);
+
+      print(historyResponse);
+
+      // Extrai os IDs das obras do histórico.
+      final obraIds = historyResponse.map((history) => history['id_history'] as int).toList();
+      if (obraIds.isEmpty) {
+        print('Nenhuma obra encontrada no histórico.');
+        return [];
+      }
+      print(obraIds.length);
+
+      // Formata os IDs em formato SQL.
+      final obraIdsFormatted = '(${obraIds.join(",")})';
+      final response = await supabase
+          .from('Obras')
+          .select('*')
+          .filter('id', 'in', obraIdsFormatted);
+
+      // Converte o resultado em uma lista de mapas para facilitar o uso.
+      return List<Map<String, dynamic>>.from(response);
+    } catch (e) {
+      print('Erro ao buscar dados: $e');
+      return [];
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final user = supabase.auth.currentUser;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Padding(
@@ -33,72 +68,136 @@ class _HistoryPageState extends State<HistoryPage> {
               style: const TextStyle(color: Colors.white),
               onChanged: (value) {
                 setState(() {
-                  searchQuery = value.toLowerCase(); // Atualiza a consulta de pesquisa
+                  searchQuery = value.toLowerCase();
                 });
               },
             ),
             const SizedBox(height: 20),
             Expanded(
               child: FutureBuilder<List<Map<String, dynamic>>>(
-                future: fetchHistoryData(),
+                future: _data(),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Center(child: CircularProgressIndicator());
                   } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text(
-                        'Error: ${snapshot.error}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    );
+                    return _buildErrorWidget(snapshot.error.toString());
+                  } else if (user == null) {
+                    return _buildNotLoggedInWidget();
                   } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(
-                      child: Text(
-                        'No history found',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    );
+                    return _buildNoHistoryFoundWidget();
                   } else {
-                    // Filtra os dados com base na consulta de pesquisa
-                    final dataList = snapshot.data!;
-                    final filteredData = dataList.where((data) {
-                      return data['Name'].toLowerCase().contains(searchQuery);
-                    }).toList();
-
-                    final screenWidth = MediaQuery.of(context).size.width;
-                    final spacing = screenWidth * 0.02; // 2% da largura da tela
-
-                    return SingleChildScrollView(
-                      child: Container(
-                        alignment: Alignment.topLeft,
-                        padding: const EdgeInsets.all(8.0),
-                        child: Wrap(
-                          spacing: spacing, // Espaçamento horizontal entre os itens
-                          runSpacing: 8.0, // Espaçamento vertical entre as linhas
-                          children: filteredData.map((data) {
-                            return SizedBox(
-                              width: 119, // Largura fixa para os cards
-                              height: 200, // Altura fixa para os cards
-                              child: MangaCard(
-                                imageUrl: data['ImageUrl'],
-                                title: data['Name'],
-                                textSize: 16,
-                                textPadding: 0,
-                                desc: '',
-                                id: data['id'],
-                                obraGenres: data['genres'],
-                                views: data['views'],
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ),
-                    );
+                    return _buildHistoryList(snapshot.data!);
                   }
                 },
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildErrorWidget(String error) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.error, color: Colors.red, size: 50),
+          const SizedBox(height: 10),
+          Text(
+            error,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.white),
+          ),
+          ElevatedButton(
+            onPressed: () => setState(() {}),
+            child: const Text('Tentar Novamente'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNotLoggedInWidget() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          'Você não está logado',
+          style: TextStyle(color: Colors.white),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'pages/login');
+          },
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(200, 40),
+            backgroundColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+          child: const Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNoHistoryFoundWidget() {
+    return const Center(
+      child: Text(
+        'Nenhum histórico encontrado',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget _buildHistoryList(List<Map<String, dynamic>> dataList) {
+    final filteredData = dataList.where((data) {
+      return data['Name'].toLowerCase().contains(searchQuery);
+    }).toList();
+
+    if (filteredData.isEmpty) {
+      return const Center(
+        child: Text(
+          'Nenhum resultado encontrado para sua pesquisa.',
+          style: TextStyle(color: Colors.white),
+        ),
+      );
+    }
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    final spacing = screenWidth * 0.02;
+
+    return SingleChildScrollView(
+      child: Container(
+        alignment: Alignment.topLeft,
+        padding: const EdgeInsets.all(8.0),
+        child: Wrap(
+          spacing: spacing,
+          runSpacing: 8.0,
+          children: filteredData.map((data) {
+            return SizedBox(
+              width: 119,
+              height: 200,
+              child: MangaCard(
+                imageUrl: data['ImageUrl'],
+                title: data['Name'],
+                textSize: 16,
+                textPadding: 0,
+                desc: '',
+                id: data['id'],
+                obraGenres: data['genres'],
+                views: data['views'],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
